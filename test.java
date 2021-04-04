75432f87a023f0138c49ac51e57e6af022f21349
KBU <gibens4@gmail.com>
2021-04-04 15:24:19 +0200

    Squashed commit of the following:
    commit 9c30c0344e55ba01ce14b4d8b77a24b78f122dc9
    Author: KBU <gibens4@gmail.com>
    Date:   Sun Apr 4 15:23:46 2021 +0200
    
        Simple support task
    
    commit cfa8fc1becd580000154d78804a5af2f19d22106
    Author: KBU <gibens4@gmail.com>
    Date:   Sun Apr 4 15:21:55 2021 +0200
    
        Simple support task

--- test.java
+++ test.java
@@ -1,17 +1,23 @@
 class Test {
     public static void main(String[] args) {
-        printTest();
-        printSquash();
+        // printTest();
+        // printSquash();
+        supportTime();
     }
 
-    private static void printTest() {
-        System.out.println("gitflow test");
-        System.out.println("important hotfix");
-    }
+    // private static void printTest() {
+    //     System.out.println("gitflow test");
+    //     System.out.println("important hotfix");
+    // }
 
-    private static void printSquash() {
-        System.out.println("commit 1");
-        System.out.println("commit 2");
-        System.out.println("commit 3");
+    // private static void printSquash() {
+    //     System.out.println("commit 1");
+    //     System.out.println("commit 2");
+    //     System.out.println("commit 3");
+    // }
+
+    private static void supportTime() {
+        System.out.println("It's hot on production right now. Jake, do something with this man.");
+        System.out.println("It's fixed, Sam. Everything should work perfectly now.");
     }
 }
