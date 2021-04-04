19eecde20b92a6748ae925a5e9262d395b68b15e
KBU <gibens4@gmail.com>
2021-04-04 15:31:20 +0200

    Simple extra feature

--- test.java
+++ test.java
@@ -2,6 +2,8 @@ class Test {
     public static void main(String[] args) {
         printTest();
         printSquash();
+        int calculated = addOneNumberToAnother(1, 1);
+        System.out.println(calculated);
     }
 
     private static void printTest() {
@@ -13,5 +15,9 @@ class Test {
         System.out.println("commit 1");
         System.out.println("commit 2");
         System.out.println("commit 3");
+    }
+
+    private static int addOneNumberToAnother(int no1, int no2) {
+        return no1 + no2;
     }
 }
